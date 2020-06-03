23b6d4d3b813e8fcf0f10830b0b7abb40021c2d9
Adam Johnson <adamwjo@gmail.com>
2020-06-03 14:09:03 -0400

    setting up user model

---
+++ models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  first_name: {
+    type: String,
+    required: true,
+  },
+  last_name: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  timestamps: {
+    createdAt: 'created_at',
+  },
+});
+
+module.exports = User = mongoose.model('user', UserSchema);
