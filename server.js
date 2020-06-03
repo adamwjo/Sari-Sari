e1ae78becaa6bec04562e43e020d66b317d1169c
Adam Johnson <adamwjo@gmail.com>
2020-06-03 13:49:07 -0400

    finished setting up initial routes

--- server.js
+++ server.js
@@ -2,11 +2,19 @@ const express = require('express');
 const connectDB = require('./config/db');
 
 const app = express();
+
+//Database connection call
 connectDB();
 
 app.get('/', (req, res) => {
   res.send('Server Running');
 });
+
+//Route Imports
+app.use('/api/v1/users', require('./routes/users'));
+app.use('/api/v1/auth', require('./routes/auth'));
+app.use('/api/v1/profiles', require('./routes/profiles'));
+app.use('/api/v1/posts', require('./routes/posts'));
 
 const PORT = process.env.PORT || 8000;
 
