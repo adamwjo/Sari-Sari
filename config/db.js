cc218bdd685f4f95d1a2ef7c8c6808538adc3806
Adam Johnson <adamwjo@gmail.com>
2020-06-05 17:56:56 -0400

    Set up basic validations for create new user

--- config/db.js
+++ config/db.js
@@ -7,6 +7,7 @@ const connectDB = async () => {
     await mongoose.connect(dbURI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      useCreateIndex: true,
     });
     console.log('DB connected');
   } catch (error) {
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,45 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const User = require('../models/User');
 
+const newUserValidations = [
+  check('first_name', 'Please submit a first name').not().isEmpty(),
+  check('email', 'Please submit a valid email').isEmail(),
+  check('password', 'Your password must be at least 6 characters').isLength({
+    min: 6,
+  }),
+];
+
+//Test route
 router.get('/', (req, res) => {
   res.send('Users Route');
+});
+
+//POST route (Create new User)
+router.post('/', newUserValidations, async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { first_name, last_name, username, email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+    if (user) {
+      res
+        .status(400)
+        .json({ errors: [{ message: 'User already exits in database' }] });
+    }
+
+    res.send('Creating New User');
+  } catch (error) {
+    console.error(error.message);
+    res.status(500);
+  }
+
+  res.send('Creating New User');
 });
 
 module.exports = router;
