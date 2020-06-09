3d3d29a21e7970d8cf0bca0bf16f7497c6892d80
Adam Johnson <adamwjo@gmail.com>
2020-06-09 11:59:35 -0400

    completed user login route

--- routes/auth.js
+++ routes/auth.js
@@ -3,15 +3,15 @@ const config = require('config');
 const router = express.Router();
 const auth = require('../middleware/auth');
 const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+
 // Import User Model
 const User = require('../models/User');
 
 const userLoginValidations = [
   check('email', 'Please submit a valid email').isEmail(),
-  check('password', 'Your password must be at least 6 characters').isLength({
-    min: 6,
-  }),
+  check('password', 'Please submit a valid password').exists(),
 ];
 
 // USER AUTH ROUTE
@@ -26,6 +26,52 @@ router.get('/', auth, async (req, res) => {
 });
 
 // USER LOGIN ROUTE
-router.post('/', userLoginValidations, (req, res) => {});
+router.post('/login', userLoginValidations, async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { email, password } = req.body;
+
+  try {
+    // -----> Check db for user by email, if no user send json error
+    let user = await User.findOne({ email });
+    if (!user) {
+      res
+        .status(400)
+        .json({ errors: [{ message: 'Invalid username or password' }] });
+    }
+    // -----> Validate Password
+    const passValidate = await bcrypt.compare(password, user.password);
+    if (!passValidate) {
+      res
+        .status(400)
+        .json({ errors: [{ message: 'Invalid username or password' }] });
+    }
+    // -----> create payload and distribute token
+    const payload = {
+      user: {
+        id: user.id,
+      },
+    };
+    jwt.sign(
+      payload,
+      config.get('jwtSecret'),
+      { expiresIn: 3600000 },
+      (error, token) => {
+        if (error) {
+          res
+            .status(401)
+            .json({ errors: [{ message: 'Token not authorized' }] });
+        }
+        res.json({ token });
+      }
+    );
+  } catch (error) {
+    console.error(error.message);
+    res.status(500);
+  }
+});
 
 module.exports = router;
