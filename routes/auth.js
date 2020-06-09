366aeb451db5082308307a2759b3dbae8b3b3595
Adam Johnson <adamwjo@gmail.com>
2020-06-09 11:03:13 -0400

    initial commit

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const config = require('config');
 const router = express.Router();
 const auth = require('../middleware/auth');
+const { check, validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
 // Import User Model
 const User = require('../models/User');
 
+const userLoginValidations = [
+  check('email', 'Please submit a valid email').isEmail(),
+  check('password', 'Your password must be at least 6 characters').isLength({
+    min: 6,
+  }),
+];
+
+// USER AUTH ROUTE
 router.get('/', auth, async (req, res) => {
   try {
     const user = await User.findById(req.user.id).select('-password');
@@ -13,5 +24,8 @@ router.get('/', auth, async (req, res) => {
     res.status(500).send('server error');
   }
 });
+
+// USER LOGIN ROUTE
+router.post('/', userLoginValidations, (req, res) => {});
 
 module.exports = router;
