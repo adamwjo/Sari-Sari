915a5ec7f37ed0872c45b2083b5ca7da4ffd9f18
Adam Johnson <adamwjo@gmail.com>
2020-06-09 10:22:37 -0400

    small fixes

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/', (req, res) => {
   res.send('Server Running');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`Server started on port:${PORT}`);
