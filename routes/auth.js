136e83b7c1c3b1a841f9d4f67d4afcced62dcd0d
Adam Johnson <adamwjo@gmail.com>
2020-06-09 10:22:37 -0400

    small fixes

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
+// Import User Model
+const User = require('../models/User');
 
-router.get('/', (req, res) => {
-  res.send('Auth Route');
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('server error');
+  }
 });
 
 module.exports = router;
